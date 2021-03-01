659017f4b04687385120d855c3a0e9f868cb64e1
Alexander <alexander.kosykh.fc@gmail.com>
2021-03-01 13:39:18 +0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { connect } from "react-redux";
+import { Todo, fetchTodos, deleteTodo } from "./store/actions";
+import { StoreState } from "./store/reducers";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface AppProps {
+  todos: Todo[];
+  fetchTodos: Function;
+  deleteTodo: Function;
 }
 
-export default App;
+interface AppState {
+  fetching: boolean;
+}
+
+class App extends React.Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+    this.state = { fetching: false };
+  }
+
+  componentDidUpdate(prevProps: AppProps): void {
+    if (!prevProps.todos.length && this.props.todos.length) {
+      this.setState({ fetching: false });
+    }
+  }
+
+  onButtonClick = (): void => {
+    setTimeout(() => {
+      this.props.fetchTodos();
+    }, 300);
+    this.setState({ fetching: true });
+  };
+
+  renderList(): JSX.Element[] {
+    return this.props.todos.map((todo: Todo) => {
+      return <div key={todo.id}>{todo.title}</div>;
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.onButtonClick}>Fetch todos</button>
+        {this.state.fetching && "LOADING"}
+        {this.renderList()}
+      </div>
+    );
+  }
+}
+
+const mapStateProps = ({ todos }: StoreState): { todos: Todo[] } => {
+  return { todos };
+};
+
+export default connect(mapStateProps, { fetchTodos, deleteTodo })(App);
