31d2f7f2c5f4c9144f4fa2e9f4fed25252595889
Alexander <alexander.kosykh.fc@gmail.com>
2021-03-14 11:41:56 +0300

    Add material/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Todo, fetchTodos, deleteTodo } from "./store/actions";
-import "./App.css";
+
+import { makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
 
 interface AppProps {
   todos: Todo[];
@@ -11,16 +15,27 @@ interface AppProps {
 //   fetching: boolean;
 // }
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
 const App = () => {
+  const classes = useStyles();
   const dispatch = useDispatch();
   const todos = useSelector((state: AppProps) => state.todos);
   useEffect(() => {
     dispatch(fetchTodos());
   }, [dispatch]);
   const todosItems: JSX.Element[] = todos.map((todo: Todo) => (
-    <div key={todo.id}>{todo.title}</div>
+    <ListItem key={todo.id} button>
+      <ListItemText primary={`Line item ${todo.title}`} />
+    </ListItem>
   ));
-  return <div>{todosItems}</div>;
+  return <List className={classes.root}>{todosItems}</List>;
 };
 
 export default App;
