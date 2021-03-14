90379187de5021ab616f404c5419e05d1bf93b10
Alexander <alexander.kosykh.fc@gmail.com>
2021-03-14 16:51:52 +0300

    Add pagination and lodash

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React, { useEffect } from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import _ from "lodash";
 import { Todo, fetchTodos, deleteTodo } from "./store/actions";
 
 import { makeStyles } from "@material-ui/core/styles";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
+import Pagination from "@material-ui/lab/Pagination";
 
 interface AppProps {
   todos: Todo[];
@@ -23,19 +25,44 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const pageSize = 10;
+
 const App = () => {
+  const [currentPage, setCurrentPage] = useState(0);
+  //let todosItems;
   const classes = useStyles();
   const dispatch = useDispatch();
   const todos = useSelector((state: AppProps) => state.todos);
   useEffect(() => {
     dispatch(fetchTodos());
   }, [dispatch]);
-  const todosItems: JSX.Element[] = todos.map((todo: Todo) => (
-    <ListItem key={todo.id} button>
-      <ListItemText primary={`Line item ${todo.title}`} />
-    </ListItem>
-  ));
-  return <List className={classes.root}>{todosItems}</List>;
+
+  const handleChange = (event: any, value: any): void => {
+    setCurrentPage(value);
+  };
+
+  const pageCount = Math.ceil(todos.length / pageSize);
+  const displayData = _.chunk(todos, pageSize)[currentPage];
+
+  const todosItems: JSX.Element[] =
+    displayData &&
+    displayData.map((todo: Todo) => (
+      <ListItem key={todo.id} button>
+        <ListItemText primary={`Line item ${todo.title}`} />
+      </ListItem>
+    ));
+
+  return (
+    <>
+      <List className={classes.root}>{todosItems}</List>
+      <Pagination
+        count={pageCount}
+        onChange={handleChange}
+        variant="outlined"
+        shape="rounded"
+      />
+    </>
+  );
 };
 
 export default App;
